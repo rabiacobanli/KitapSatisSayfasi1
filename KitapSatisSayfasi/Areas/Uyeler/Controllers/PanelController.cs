7665712e661cb984aa3a619fd9f9ad40c8e80b5d
Rabia ÇOBANLI <r.cobanli95@hotmail.com>
2022-07-05 17:54:39 +0300

    Add project files.

---
+++ KitapSatisSayfasi/Areas/Uyeler/Controllers/PanelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitapSatisSayfasi.Areas.Uyeler.Controllers
+{
+    [Area("Uyeler")]
+    public class PanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KitapSatisSayfasi/Models/Configuration/UyeCFG.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KitapSatisSayfasi.Models.Configuration
+{
+    public class UyeCFG : IEntityTypeConfiguration<Uye>
+    {
+        public void Configure(EntityTypeBuilder<Uye> builder)
+        {
+            Uye uye = (new Uye() { Id = 1,Ad = "Cevdet",Soyad = "Korkmaz",UserName = "Cevdo", NormalizedUserName = "CEVDET", Email = "cevdo@deneme.com", NormalizedEmail = "cevdo@deneme.com", EmailConfirmed =false, Adres = "Kadıköy" });
+            PasswordHasher<Uye> hash = new PasswordHasher<Uye>();
+            uye.PasswordHash = hash.HashPassword(uye, "123");
+            builder.HasData(uye);
+            
+            
+        }
+    }
+}
